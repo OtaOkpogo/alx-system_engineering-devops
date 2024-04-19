88978b2e4b937aa601ce0b8ef767d710d7d42e9e
OtaOkpogo <Otaokpogo@yahoo.com>
2024-04-19 13:21:50 -0700

    updated 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Define a package resource for Flask
 package { 'Flask':
-  ensure   => '2.1.0',   # Ensure version 2.1.0 is installed
-  provider => 'pip3',    # Use pip3 as the package provider
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
